06f005b7c2685f5d925df749a5dbd5ba828cbc7d
fevse <fedulov.v.s@gmail.com>
2023-02-04 17:19:45 +0600

    add cat and dog

---
+++ typeanddrow/scripts/cat.py
@@ -0,0 +1,14 @@
+import time
+
+def main():
+    time.sleep(1)
+    print('/\\_/\\')
+    time.sleep(1)
+    print('>^,^<')
+    time.sleep(1)
+    print(' / \\')
+    time.sleep(1)
+    print('(|_|)_/')
+
+if __name__=='__main__':
+    main()
